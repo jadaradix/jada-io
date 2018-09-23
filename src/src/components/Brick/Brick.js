3d334f2f6ccee97c0158148789c0b6e1ba883f7c
James Garner <j@jada.io>
2018-09-23 12:51:36 +0100

    line-height, sample content

--- src/src/components/Brick/Brick.js
+++ src/src/components/Brick/Brick.js
@@ -1,23 +1,52 @@
-import React, { Component } from 'react';
-import styled, { css } from 'styled-components'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
 
 const StyledBrick = styled.section`
-  padding: 1rem;
-  background-color: rgb(248, 248, 248);
-  color: rgb(48, 48, 48);
-  border-radius: 4px;
-  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
+  padding: 1.5rem;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
   font-weight: 400;
   text-size-adjust: 100%;
   -webkit-font-smoothing: antialiased;
   letter-spacing: 0.1px;
+  border-radius: 4px;
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
+  h1 {
+    font-size: 2rem;
+    font-weight: 800;
+    color: rgb(96, 96, 96);
+    line-height: 3rem;
+    // background-color: green;
+  }
+  p {
+    color: rgb(48, 48, 48);
+    line-height: 1.2rem;
+    // background-color: red;
+  }
+  h1 + p {
+    margin-top: 0.5rem;
+  }
+  p + p {
+    margin-top: 1rem;
+  }
 `;
 
-const Brick = ({ children }) => (
-  <StyledBrick>
+const Brick = ({ children, backgroundColor }) => (
+  <StyledBrick style={{backgroundColor}}>
     {children}
   </StyledBrick>
 );
+
+Brick.propTypes = {
+  backgroundColor: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+};
+
+Brick.defaultProps = {
+  backgroundColor: 'rgb(250, 250, 250)'
+};
 
 export default Brick;
--- src/src/index.js
+++ src/src/index.js
@@ -9,7 +9,10 @@ class App extends PureComponent {
     return (
       <main>
         <Brick>
-          <p>Brick 1</p>
+          <h1>James Garner</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus ullamcorper elit, nec ultricies eros tempus ut. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris vestibulum lacus in ipsum sollicitudin, et bibendum purus rhoncus.</p>
+          <p>Nulla feugiat rutrum suscipit. Praesent consectetur ornare dolor, in viverra nulla placerat et. Nam tincidunt placerat purus vitae fermentum. Vestibulum dolor ante, placerat ut porta in, vulputate sed nisl. In mauris erat, porttitor nec ornare non, mollis non tellus. Praesent pulvinar quis magna rhoncus blandit.</p>
+          <p>Praesent commodo cursus eros ut sollicitudin. Sed elementum, tortor sed tempus pretium, enim leo aliquam neque, ut hendrerit neque diam a tellus. Aenean eu eros velit.</p>
         </Brick>
         <Brick>
           <p>Brick 2</p>
